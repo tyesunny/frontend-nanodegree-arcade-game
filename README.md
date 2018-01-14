973e5ebc7c6a6cf03dfa432d29f1cfff14e7e3c0
Tang Ye <tanielye@gmail.com>
2018-01-13 20:55:46 -0500

    Implement game logic

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-frontend-nanodegree-arcade-game
-===============================
+# Arcade Game
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+## Instructions
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+* Download the project and open index.html to start playing.
+
+* Goals/Winning
+** In this game the goal is simple, get the hero character to move across the gray bricks towards the water without getting hit by the enemy bugs. The user's score increases by one point for each level passed. The number of enemy bugs also increase with each level and are randomly placed across the gray bricks at the start of each game.
+
+* Movement and Controls
+** Users move the hero character by using the main up/right/left/down arrow keys on their keyboard.
+
+
+
+## Notice
+
+This project is done as part of the Front-End Web Developer Nanodegree program by [Udacity](https://www.udacity.com). If you are like me and rather bad with styling, but wish to create your own version of the game, feel free to grab the starter files [here]https://github.com/udacity/frontend-nanodegree-arcade-game.
--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// constant describing map grid
+const COL_STEP    = 101
+const ROW_STEP    = 83
+const COL         = 5
+const ROW         = 6
 
+// Enemies our player must avoid
+var Enemy = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,7 +20,26 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+    // make enemies loop to left side of canvas after reaching canvas.width
+    if (this.x >= ROW*ROW_STEP) {
+        this.x = 0;
+    }
+    // check collision
+    if (this.isCollidedWithPosition(player.x, player.y))
+    {
+      // collision happens, reset player position
+      player.lose+=1;
+      player.reset_position();
+      console.log(`Lose: ${player.lose} times`);
+    }
 };
+
+Enemy.prototype.isCollidedWithPosition = function(x, y) {
+    return Math.abs(this.x - x) < COL_STEP/2 && Math.abs(this.y - y) == 0;
+}
+
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -22,18 +47,84 @@ Enemy.prototype.render = function() {
 };
 
 // Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+var Player = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.win  = 0;
+    this.lose = 0;
+    this.sprite = 'images/char-boy.png';
+};
 
+// update player position when it hit the map edge
+Player.prototype.update = function(dt) {
+    if (this.x >= (COL-1)*COL_STEP) {
+        this.x =  (COL-1)*COL_STEP;
+    }
+    if (this.x <= 0) {
+        this.x =  0;
+    }
+    if (this.y >= (ROW-1)*ROW_STEP) {
+        this.y =  (ROW-1)*ROW_STEP;
+    }
+    if (this.y < 0) {
+        this.win += 1;
+        this.reset_position();
+        this.reset_enemies();
+        document.querySelector("h1").textContent = `Level: ${this.win}`;
+        console.log(`Win: ${this.win} times`);
+    }
+};
+
+// reset player position
+Player.prototype.reset_position = function() {
+    this.x = (COL-1)/2*COL_STEP
+    this.y = (ROW-1)*ROW_STEP
+}
+
+Player.prototype.reset_enemies = function() {
+  // clear enemies
+  allEnemies.length = 0;
+  // create enemies according to winning times
+  for (let i = 0; i <= this.win; i++)
+  {
+    var enemy = new Enemy(0, Math.floor(Math.random() * 4 + 1)*ROW_STEP, Math.random() * 256);
+    allEnemies.push(enemy);
+  }
+
+}
+
+// render player icon
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// handler for key press
+Player.prototype.handleInput = function(keyPress) {
+    if (keyPress == 'left') {
+        this.x -= COL_STEP
+    }
+    if (keyPress == 'up') {
+        this.y -= ROW_STEP;
+    }
+    if (keyPress == 'right') {
+        this.x += COL_STEP;
+    }
+    if (keyPress == 'down') {
+        this.y += ROW_STEP;
+    }
+    console.log('keyPress is: ' + keyPress);
+};
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var player = new Player((COL-1)/2*COL_STEP, (ROW-1)*ROW_STEP);
+// create enemy w. x = 0 y = row#0/1/2/3
+var enemy = new Enemy(0, Math.floor(Math.random() * 4 + 1)*ROW_STEP, Math.random() * 256);
+// Place all enemy objects in an array called allEnemies
+var allEnemies = [];
+allEnemies.push(enemy);
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -41,6 +132,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
